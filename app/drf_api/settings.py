388d0efebc54034456d928222f11c7fc7b7d3e1b
Les Borkowski <leszek.borkowski@outlook.com>
2022-03-17 16:35:47 +0000

    postgres and docker setup

--- app/drf_api/settings.py
+++ app/drf_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g+3#-xmw)-%^j_b87^!hd14!v*chrl)8c$*g&^lf6hc%#k@m80'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -76,9 +79,13 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
---
+++ todos.md
@@ -0,0 +1,28 @@
+# Anchovy API (DRF)
+
+## TODO
+
+### SETUP
+**Initial**
+- [x] django
+- [x] django rest framework
+- [x] postgres
+- [x] docker
+    - [x] drf dockerfile
+    - [x] postgres
+    - [] (pgadmin)
+- [] pytest
+
+### MODELS
+- [x] custom user
+- [] group
+- [] event
+- [] post
+
+- Join tables
+- [] members
+- [] events
+- [] posts
+
+### AUTH
+- [] setup
